2c17450c57345351148349cf484ea861a63b979e
Shrishti Mishra <imshrishti0212@gmail.com>
2025-06-29 02:54:45 +0530

    hopefully last.

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: process.env.REACT_APP_API_BASE_URL|| "http://localhost:5000",
   headers: {
     "Content-Type": "application/json",
   },

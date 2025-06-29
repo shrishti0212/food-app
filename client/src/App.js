a6fb620a2b2e39d07eba76e10d57023f422ca095
Shrishti Mishra <imshrishti0212@gmail.com>
2025-06-29 15:36:39 +0530

    last.👍👍

--- client/src/App.js
+++ client/src/App.js
@@ -2,91 +2,116 @@ import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header.js";
 import Body from "./components/Body.js";
-import RestaurantCard from "./components/RestaurantCard.js";
-import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import RestaurantMenu from "./components/RestaurantMenu.js";
 import Contact from "./components/Contact.js";
 import Error from "./components/Error.js";
-import RestaurantMenu from "./components/RestaurantMenu.js";
-import { Provider } from "react-redux";
-import appStore from "./utils/appStore.js";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
 import Cart from "./components/Cart.js";
 import PaymentSuccessful from "./components/PaymentSuccessful.js";
 import Login from "./components/Login.js";
 import Register from "./components/Register.js";
 import { useLocation } from "react-router-dom";
-import  SplashScreen  from "./components/SplashScreen.js";
+import SplashScreen from "./components/SplashScreen.js";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore.js";
+import ProtectedRoute from "./components/ProtectedRoute.js";
+import Grocery from "./components/Grocery.js";
+import About from "./components/About.js"
 
-const Grocery = lazy(() => import("./components/Grocery.js")); 
-const About = lazy(() => import("./components/About.js"  ));
+const AppLayout = () => {
+  const location = useLocation();
+  const hideHeaderRoutes = ["/", "/login", "/register"];
+  const shouldHideHeader = hideHeaderRoutes.includes(location.pathname);
 
-const AppLayout =()=> {
-
-     const location = useLocation();
-     const hideHeaderRoutes = ["/login", "/register","/"];
-     const shouldHideHeader = hideHeaderRoutes.includes(location.pathname);
-
-    return(
-       <Provider store={appStore}>
-        <div className="app">
-          {!shouldHideHeader && <Header />}
-            < Outlet/>
-        </div>
-        </Provider>
-    );
+  return (
+    <Provider store={appStore}>
+      <div className="app">
+        {!shouldHideHeader && <Header />}
+        <Outlet />
+      </div>
+    </Provider>
+  );
 };
-
 
 const appRouter = createBrowserRouter([
   {
     path: "/",
-    element: <AppLayout/>,
+    element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       
       {
-        path:"/home",
-        element: <Body/>,
+        path: "/",
+        element: <SplashScreen />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+
+      
+      {
+        path: "/home",
+        element: (
+          <ProtectedRoute>
+            <Body />
+          </ProtectedRoute>
+        ),
       },
       {
         path: "/about",
-        element: <About/>
+        element: (
+          <ProtectedRoute>
+              <About />
+          </ProtectedRoute>
+        ),
       },
       {
-        path:"/contact",
-        element:<Contact/>,
-      }, 
+        path: "/contact",
+        element: (
+          <ProtectedRoute>
+            <Contact />
+          </ProtectedRoute>
+        ),
+      },
       {
         path: "/grocery",
-        element: <Suspense fallback = {<h1>Loading.....</h1>} ><Grocery /></Suspense>,
+        element: (
+          <ProtectedRoute>
+              <Grocery />
+          </ProtectedRoute>
+        ),
       },
       {
-        path:"/restaurants/:resId",
-        element:<RestaurantMenu/>,
+        path: "/restaurants/:resId",
+        element: (
+          <ProtectedRoute>
+            <RestaurantMenu />
+          </ProtectedRoute>
+        ),
       },
       {
-        path:"/cart",
-        element:<Cart/>,
+        path: "/cart",
+        element: (
+          <ProtectedRoute>
+            <Cart />
+          </ProtectedRoute>
+        ),
       },
       {
-        path:"/payment-success",
-        element: <PaymentSuccessful/>,
+        path: "/payment-success",
+        element: (
+          <ProtectedRoute>
+            <PaymentSuccessful />
+          </ProtectedRoute>
+        ),
       },
-      {
-        path:"/login",
-        element:<Login/>,
-      },
-      {
-        path:"/register",
-        element:<Register/>,
-      },
-      {
-         path: "/",
-         element: <SplashScreen />,
-      }
     ],
-    errorElement:<Error/>,
   },
-  
-  ]
-);
+]);
 
 export default appRouter;
